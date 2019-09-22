cd82235e775a233e1c9f1262e301f5c23a4a85ec
Natalia Bandeira <nataliapbandeira@gmail.com>
2019-09-22 20:19:42 -0300

    Link with like/dislike api, message when there is no more devs on list, adjust on css when showing message.

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,119 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logoTindev.svg';
 import like from '../assets/likeTindev.svg';
 import dislike from '../assets/dislikeTindev.svg';
 
 export default function Main({ match }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUser() {
+            const response = await api.get('/devs', {
+                headers: {
+                    user: match.params.id,
+                }
+            });
+
+            setUsers(response.data);
+        }
+
+        loadUser();
+    }, [match.params.id]);
+
+    async function handleLike(id) {
+        await api.post(`devs/${id}/likes`, null, {
+            headers: { user: match.params.id },
+        });
+
+        setUsers(users.filter(user => user._id != id));
+    }
+
+    async function handleDislike(id) {
+        await api.post(`devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id },
+        })
+
+        setUsers(users.filter(user => user._id != id));
+    }
+
     return (
         <div className="main-container">
-            <img src={logo} alt="Tindev" />
-            <ul>
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/18669373?v=4" alt="" />
-                    <footer>
-                        <strong>Natalia Paula</strong>
-                        <p> Programmer and gamer on free time.</p>
-                    </footer>
+            <Link to="/" >
+                <img src={logo} alt="Tindev" />
+            </Link>
+            {users.length > 0 ? (
+                <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt={user.name} />
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
 
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/18669373?v=4" alt="" />
-                    <footer>
-                        <strong>Natalia Paula</strong>
-                        <p> Programmer and gamer on free time.</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/18669373?v=4" alt="" />
-                    <footer>
-                        <strong>Natalia Paula</strong>
-                        <p> Programmer and gamer on free time.</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/18669373?v=4" alt="" />
-                    <footer>
-                        <strong>Natalia Paula</strong>
-                        <p> Programmer and gamer on free time.</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/18669373?v=4" alt="" />
-                    <footer>
-                        <strong>Natalia Paula</strong>
-                        <p> Programmer and gamer on free time.</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://avatars1.githubusercontent.com/u/18669373?v=4" alt="" />
-                    <footer>
-                        <strong>Natalia Paula</strong>
-                        <p> Programmer and gamer on free time.</p>
-                    </footer>
-
-                    <div className="buttons">
-                        <button type="button" >
-                            <img src={like} alt="Like" />
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt="Dislike" />
-                        </button>
-                    </div>
-                </li>
-
-
-            </ul>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleLike(user._id)} >
+                                    <img src={like} alt="Like" />
+                                </button>
+                                <button type="button" onClick={() => handleDislike(user._id)} >
+                                    <img src={dislike} alt="Dislike" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                    <div className="empty">
+                        No more devs on your list! :(
+                </div>
+                )}
         </div>
     )
 }
